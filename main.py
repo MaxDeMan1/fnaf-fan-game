7035f6dce00648ee9980b9dd111cb0e16ae3ee72
MaxDeMan1 <maxburna4@gmail.com>
2026-03-21 11:59:42 +1100

    Add doors

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400), pygame.SCALED | pygame.RESIZABLE)
 clock = pygame.time.Clock()
 running = True
+
+office : pygame.Surface = pygame.image.load("/home/max/Pictures/fnaf/office.png")
 left_door_area = pygame.Rect(0, 120, 185, 300)
+right_vent_area = pygame.Rect(610, 125, 125, 115)
 
 while running:
     # Poll for events
@@ -15,15 +18,16 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 if left_door_area.collidepoint(event.pos):
-                    pass
+                    print("left")
+                if right_vent_area.collidepoint(event.pos):
+                    print("right")
 
     # Fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
 
-    # RENDER YOUR GAME HERE
-    office : pygame.Surface = pygame.image.load("/home/max/Pictures/fnaf/office.png")
+    # Rendering
     screen.blit(office)
-    # pygame.draw.rect(screen, (100, 200, 50), left_door_area)
+    # pygame.draw.rect(screen, (100, 200, 50), right_vent_area) # TODO: REMOVE
 
     pygame.display.flip() # Puts work on screen
     delta = clock.tick() / 1000 # Sets delta time
